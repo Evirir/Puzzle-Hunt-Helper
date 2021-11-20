3424ace10347d733c482cc178c0ed3e48c6cd079
Evirir <gojunxing@gmail.com>
2021-11-19 23:14:51 -0800

    Beautify code, add GitHub to help

--- src/commands/Utility/help.ts
+++ src/commands/Utility/help.ts
@@ -1,39 +1,40 @@
-import {Message, MessageEmbed} from 'discord.js';
-import fs from 'fs';
-import {defaultPrefix} from '../../data.json';
-import {BotClient, Command, CommandArguments} from "../../types";
+import fs from "fs";
 import path from "path";
+import {Message, MessageEmbed} from "discord.js";
+import {bold, hyperlink, inlineCode} from "@discordjs/builders";
+import {defaultPrefix, sourceLink} from "../../data.json";
+import {BotClient, Command, CommandArguments} from "../../types";
 
 const command: Command = {
-    name: 'help',
-    description: `Show list of commands if used without arguments. Use \`${defaultPrefix}help [command]\` for more info on the command.`,
-    aliases: ['commands', 'command', 'cmd'],
-    usage: '(command-name)',
+    name: "help",
+    description: "Show list of commands if used without arguments. Use " + inlineCode(`${defaultPrefix}help [command]`) + " for more info on the command.",
+    aliases: ["commands", 'command', 'cmd'],
+    usage: "(command-name)",
 
     async execute(message: Message, args: CommandArguments, prefix: string) {
         const mainArgs = args.main;
 
         if (!mainArgs.length) {
             const embed = new MessageEmbed()
-                .setColor('BLUE')
-                .setDescription('My prefix is \`' + prefix + '\`.\n' +
-                    '\`!m [meta] -l [link]\` to create a new meta (category).\n' +
-                    '\`!p [puzzle] -l [link]\` to create a puzzle (channel) in your current category.\n' +
-                    '\`!s [answer]\` to mark a puzzle as solved.\n\n' +
-                    '**List of commands:**')
+                .setColor("BLUE")
+                .setDescription("My prefix is " + inlineCode(prefix) + ".\n" +
+                    inlineCode("!m [meta] -l [link]") + " to create a new meta (category).\n" +
+                    inlineCode("!p [puzzle] -l [link]") + " to create a puzzle (channel) in your current category.\n" +
+                    inlineCode("!s [answer]") + " to mark a puzzle as solved.\n\n" +
+                    "Source code: " + hyperlink("GitHub link", sourceLink) + "\n\n" +
+                    bold("List of commands:"))
                 .setFooter(`Type ${prefix}help [command] for more info on the command.`);
 
-            const categories = fs.readdirSync(path.resolve(__dirname, '..'));
-            console.log(categories);
+            const categories = fs.readdirSync(path.resolve(__dirname, ".."));
             categories.forEach(category => {
                 const commandFiles = fs.readdirSync(path.resolve(__dirname, `../${category}`))
-                                        .filter(file => file.endsWith('.ts') || file.endsWith('.js'));
-                let commandArray: Command[] = [];
+                    .filter(file => file.endsWith(".ts") || file.endsWith(".js"));
+                let commandArray: string[] = [];
                 commandFiles.forEach(file => {
-                    const command = require(`../${category}/${file}`);
-                    commandArray.push(command.name);
+                    const command: Command = require(`../${category}/${file}`);
+                    commandArray.push(inlineCode(command.name));
                 });
-                embed.addField(`${category}`, `\`${commandArray.join('\` \`')}\``);
+                embed.addField(category, commandArray.join(" "));
             });
 
             await message.channel.send({embeds: [embed]});
@@ -46,23 +47,23 @@ const command: Command = {
                 return message.reply("Unknown command.");
 
             const embed = new MessageEmbed()
-                .setTitle(`\`${command.name}\``)
-                .setColor('BLUE');
+                .setTitle(inlineCode(command.name))
+                .setColor("BLUE");
 
             if (command.aliases)
-                embed.addField(`Aliases`, `\`${command.aliases.join('\` \`')}\``);
+                embed.addField("Aliases", command.aliases.map(alias => inlineCode(alias)).join(" "));
             if (command.description)
-                embed.addField(`Description`, command.description);
+                embed.addField("Description", command.description);
             if (command.usage)
-                embed.addField(`Usage`, `\`${prefix}${command.name} ${command.usage}\``);
+                embed.addField("Usage", inlineCode(prefix + command.name + " " + command.usage));
             if (command.notes)
-                embed.addField(`Notes`, command.notes);
+                embed.addField("Notes", command.notes);
             if (command.args) {
                 let argsDescription: string = "";
                 for (const [key, value] of Object.entries(command.args)) {
-                    argsDescription += `\`-${key}\`: ${value.description}\n`;
+                    argsDescription += inlineCode("-" + key) + ": " + value.description + "\n";
                 }
-                embed.addField('Additional arguments', argsDescription);
+                embed.addField("Additional arguments", argsDescription);
             }
 
             await message.channel.send({embeds: [embed]});
--- src/commands/Utility/invite.ts
+++ src/commands/Utility/invite.ts
@@ -1,14 +1,16 @@
 import {inviteLink} from '../../data.json';
-import {Message} from "discord.js";
+import {Message, MessageEmbed} from "discord.js";
 import {Command} from "../../types";
+import {hyperlink} from "@discordjs/builders";
 
 const command: Command = {
     name: 'invite',
-    description: `Get the invite link of this bot.`,
+    description: "Get the invite link of this bot.",
 
     execute(message: Message) {
-        message.channel.send("Invite link: " + inviteLink);
+        const embed = new MessageEmbed().setColor("GREEN").setDescription(hyperlink("Summon me to your server!", inviteLink));
+        message.channel.send({embeds: [embed]});
     }
-};
+}
 
-module.exports = command;
+    module.exports = command;
